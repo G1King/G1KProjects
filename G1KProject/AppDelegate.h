fb65703f4cf5ca9d5015277fbd27136db1a7a5b7
wangzhaoshuai <zhaoshuai.wang@bjsobf.com>
2016-11-02 10:26:12 +0800

    Initial Commit

---
+++ G1KProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G1KProject
+//
+//  Created by Sobf Leo on 16/11/2.
+//  Copyright © 2016年 SOBF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G1KProjectTests/G1KProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  G1KProjectTests.m
+//  G1KProjectTests
+//
+//  Created by Sobf Leo on 16/11/2.
+//  Copyright © 2016年 SOBF. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface G1KProjectTests : XCTestCase
+
+@end
+
+@implementation G1KProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
